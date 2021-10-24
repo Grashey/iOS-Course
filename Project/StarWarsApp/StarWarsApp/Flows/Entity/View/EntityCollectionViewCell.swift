c4d5713d971f6097410f31d5d4436951dbe7dfef
Grashey <grashey@mail.ru>
2021-10-24 12:46:25 +0300

    Добавил EntityCollectionViewCell

---
+++ Project/StarWarsApp/StarWarsApp/Flows/Entity/View/EntityCollectionViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  EntityCollectionViewCell.swift
+//  StarWarsApp
+//
+//  Created by Aleksandr Fetisov on 22.10.2021.
+//
+
+import UIKit
+
+class EntityCollectionViewCell: UICollectionViewCell {
+    
+    let inset: CGFloat = 10
+    
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: Constants.Fonts.font, size: 10)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textColor = .white
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.backgroundColor = .black
+        addSubviews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(label)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: inset/2),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+        ])
+    }
+    
+    func configureWith(name: String, image: UIImage, imageSize: CGSize) {
+        self.imageView.image = resizeImage(image: image, targetSize: imageSize)
+        self.label.text = name
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        let sideRatio = targetSize.width / size.width
+        let newSize = CGSize(width: size.width * sideRatio, height: size.height * sideRatio)
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
