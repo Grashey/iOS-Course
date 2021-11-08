bf340ce4935aa0bd79c826f7b463ef95b0b50a99
Grashey <grashey@mail.ru>
2021-11-08 12:40:34 +0300

    Вынес логику спиннера в отдельный класс

---
+++ Project/StarWarsApp/StarWarsApp/Flows/Other/Spinner/SpinnerManager.swift
@@ -0,0 +1,37 @@
+//
+//  SpinnerManager.swift
+//  StarWarsApp
+//
+//  Created by Aleksandr Fetisov on 06.11.2021.
+//
+
+import UIKit
+
+class SpinnerManager: UIViewController {
+    
+    private let spinner = SpinnerViewController()
+    
+    var isLoading = false {
+        didSet {
+            guard oldValue != isLoading else { return }
+            showSpinner(isShown: isLoading)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        spinner.view.frame = view.frame
+    }
+    
+    private func showSpinner(isShown: Bool) {
+        if isShown {
+            addChild(spinner)
+            view.addSubview(spinner.view)
+            spinner.didMove(toParent: self)
+        } else {
+            spinner.willMove(toParent: nil)
+            spinner.view.removeFromSuperview()
+            spinner.removeFromParent()
+        }
+    }
+}
