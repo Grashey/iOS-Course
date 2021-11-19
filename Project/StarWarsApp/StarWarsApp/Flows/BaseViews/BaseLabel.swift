f0eea78cd29bf2294502ceeaf8db1e0ff3299898
Grashey <grashey@mail.ru>
2021-11-19 23:03:58 +0300

    Добавил отступы для базовых классов Лейбл

--- Project/StarWarsApp/StarWarsApp/Flows/BaseViews/BaseLabel.swift
+++ Project/StarWarsApp/StarWarsApp/Flows/BaseViews/BaseLabel.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class BaseLabel: UILabel {
 
-    private let smallFont = UIFont(name: Constants.Fonts.font, size: 12)
+    private let smallFont = UIFont(name: Constants.Fonts.font, size: 14)
     private let labelTextColor: UIColor = .white
+    private let topInset: CGFloat = 10
+    private let leftInset: CGFloat = 10
+    private let bottomInset: CGFloat = 10
+    private let rightInset: CGFloat = 10
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,6 +24,18 @@ class BaseLabel: UILabel {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        var intrinsicSuperViewContentSize = super.intrinsicContentSize
+        intrinsicSuperViewContentSize.height += topInset + bottomInset
+        intrinsicSuperViewContentSize.width += leftInset + rightInset
+        return intrinsicSuperViewContentSize
     }
     
 }
--- Project/StarWarsApp/StarWarsApp/Flows/BaseViews/BaseValueLabel.swift
+++ Project/StarWarsApp/StarWarsApp/Flows/BaseViews/BaseValueLabel.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class BaseValueLabel: UILabel {
     
-    private let smallFont = UIFont(name: Constants.Fonts.font, size: 12)
+    private let smallFont = UIFont(name: Constants.Fonts.font, size: 14)
     private let labelTextColor: UIColor = .white
+    private let topInset: CGFloat = 10
+    private let leftInset: CGFloat = .zero
+    private let bottomInset: CGFloat = 10
+    private let rightInset: CGFloat = 10
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,5 +26,17 @@ class BaseValueLabel: UILabel {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        var intrinsicSuperViewContentSize = super.intrinsicContentSize
+        intrinsicSuperViewContentSize.height += topInset + bottomInset
+        intrinsicSuperViewContentSize.width += leftInset + rightInset
+        return intrinsicSuperViewContentSize
     }
 }
